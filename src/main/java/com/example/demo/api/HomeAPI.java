c308c872675caf1687952d47b806b78f1f736673
lgliu <lgliu@thoughtworks.com>
2017-10-19 12:11:20 +0800

    modify gitignore

--- src/main/java/com/example/demo/api/HomeAPI.java
+++ src/main/java/com/example/demo/api/HomeAPI.java
@@ -2,14 +2,17 @@ package com.example.demo.api;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by ligen on 2017/10/18.
  */
-@Controller
+@RestController
 @RequestMapping(value = "/")
 public class HomeAPI {
 
+    @RequestMapping(value = "", method = RequestMethod.GET)
     public String home(){
         return "hello";
     }
